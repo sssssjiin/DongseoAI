194d1e0e7c084ec771f79642f97f1ec6d1deb0ae
JB-Lee <ttdlwhd@gmail.com>
2020-08-24 23:37:15 +0900

    add GPIO support

--- rasp_server/api.py
+++ rasp_server/api.py
@@ -1,23 +1,57 @@
-from flask_restful import Resource
+import RPi.GPIO as GPIO
+from flask_restful import Resource, reqparse
+
+LED_PIN = 1
+MOTOR_PIN = 2
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(MOTOR_PIN, GPIO.OUT)
+
+pwm = GPIO.PWM()
+pwm.start(0)
 
 
 class LEDController:
     class On(Resource):
-        pass
+        @staticmethod
+        def get():
+            try:
+                GPIO.output(LED_PIN, True)
+                return {"status": "Success"}
+            except Exception as e:
+                return {"status": "Fail", "reason": str(e)}
 
     class Off(Resource):
-        pass
+        @staticmethod
+        def get():
+            try:
+                GPIO.output(LED_PIN, False)
+                return {"status": "Success"}
+            except Exception as e:
+                return {"status": "Fail", "reason": str(e)}
 
 
 class MotorController:
+    speed = 0
+
     class SetSpeed(Resource):
-        pass
+        @staticmethod
+        def get():
+            try:
+                parser = reqparse.RequestParser()
+                parser.add_argument("speed", type=int)
+                args = parser.parse_args()
+                MotorController.speed = args.get("speed", 0)
+                pwm.ChangeDutyCycle(MotorController.speed)
+                return {"status": "Success"}
+            except Exception as e:
+                return {"status": "Fail", "reason": str(e)}
 
     class GetSpeed(Resource):
-        pass
-
-    class On(Resource):
-        pass
-
-    class Off(Resource):
-        pass
+        @staticmethod
+        def get():
+            try:
+                return {"status": "Success", "value": MotorController.speed}
+            except Exception as e:
+                return {"status": "Fail", "reason": str(e)}
