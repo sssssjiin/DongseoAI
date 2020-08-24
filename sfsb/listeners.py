68fa6069cc4d7e7a29946022b1d9347bbd1bbd2a
JB-Lee <ttdlwhd@gmail.com>
2020-08-24 20:55:14 +0900

    Listener add

--- sfsb/listeners.py
+++ sfsb/listeners.py
@@ -2,39 +2,54 @@ import cortex.api as cortex
 
 
 class PowerListener(cortex.Listener):
-    def __init__(self):
-        pass
+    cols: list
+
+    def __init__(self, callback):
+        self.callback = callback
 
     @cortex.Listener.handler(cortex.ID.SUBSCRIPTION.SUBSCRIBE)
     def handle_subscribe(self, data):
-        pass
+        for stream in data["success"]:
+            if stream["streamName"] == "pow":
+                self.cols = stream["cols"]
 
     @cortex.Listener.handler("pow")
     def handle_pow(self, data):
-        pass
+        dat = {k: v for (k, v) in zip(self.cols, data["pow"])}
+        self.callback(dat)
 
 
 class MetricListener(cortex.Listener):
-    def __init__(self):
-        pass
+    cols: list
+
+    def __init__(self, callback):
+        self.callback = callback
 
     @cortex.Listener.handler(cortex.ID.SUBSCRIPTION.SUBSCRIBE)
     def handle_subscribe(self, data):
-        pass
+        for stream in data["success"]:
+            if stream["streamName"] == "met":
+                self.cols = stream["cols"]
 
     @cortex.Listener.handler("met")
     def handle_metric(self, data):
-        pass
+        dat = {k: v for (k, v) in zip(self.cols, data["met"])}
+        self.callback(dat)
 
 
 class MotionListener(cortex.Listener):
-    def __init__(self):
-        pass
+    cols: list
+
+    def __init__(self, callback):
+        self.callback = callback
 
     @cortex.Listener.handler(cortex.ID.SUBSCRIPTION.SUBSCRIBE)
     def handle_subscribe(self, data):
-        pass
+        for stream in data["success"]:
+            if stream["streamName"] == "mot":
+                self.cols = stream["cols"]
 
     @cortex.Listener.handler("mot")
     def handle_metric(self, data):
-        pass
+        dat = {k: v for (k, v) in zip(self.cols, data["mot"])}
+        self.callback(dat)
