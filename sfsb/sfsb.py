652ea310267ca457b08b3e306ec40feb988b752c
JB-Lee <ttdlwhd@gmail.com>
2020-08-24 22:36:11 +0900

    add metrics

--- sfsb/sfsb.py
+++ sfsb/sfsb.py
@@ -6,6 +6,12 @@ import cortex
 import listeners
 from global_variable import Config
 
+from pprint import pprint
+
+
+# import logging
+
+# logging.basicConfig(level=logging.DEBUG)
 
 class Callbacks:
     __eye_timestamp = list()
@@ -18,7 +24,7 @@ class Callbacks:
 
         flicker = (right + left) / 2
 
-        if len(cls.__eye_timestamp) > 20:
+        if len(cls.__eye_timestamp) > 10:
             cls.__eye_timestamp.pop(0)
             cls.__eye_warning.pop(0)
 
@@ -27,18 +33,18 @@ class Callbacks:
         eye_flicker_std = np.std(cls.__eye_timestamp)
         eye_flicker_mean = np.mean(cls.__eye_timestamp)
 
-        if 10 < eye_flicker_mean < 30 and eye_flicker_std < 15:
+        if 20 < eye_flicker_mean:
             cls.__eye_warning.append(True)
         else:
             cls.__eye_warning.append(False)
 
-        if cls.__eye_warning.count(True) > 15:
-            # 경고
+        if cls.__eye_warning.count(True) > 5:
+            # TODO 경고 메세지 전송
             print("경고")
 
     @classmethod
     def met_handler(cls, data):
-        pass
+        pprint(data["foc"])
 
     @classmethod
     def mot_handler(cls, data):
@@ -47,13 +53,14 @@ class Callbacks:
 
 async def main():
     token, session = await api.prepare()
-    await api.subscribe(token, session, ["pow"])
-    await asyncio.sleep(60)
-    await api.unsubscribe(token, session, ["pow"])
+    await api.subscribe(token, session, ["pow", "met"])
+    await asyncio.sleep(600)
+    await api.unsubscribe(token, session, ["pow", "met"])
 
 
 api = cortex.Wrapper(client_id=Config.emotiv.get("Client_ID"), client_secret=Config.emotiv.get("Client_Secret"),
                      main=main)
 api.register_listener(listeners.PowerListener(Callbacks.pow_handler))
+api.register_listener(listeners.MetricListener(Callbacks.met_handler))
 
 api.run()
